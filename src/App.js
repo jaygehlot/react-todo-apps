a72281f1c4175b764342710e6cefcb2d1ab357f5
Jay Gehlot <jay.gehlot2@vodafone.com>
2020-12-09 18:20:50 +0000

    Add to the css file and finish taking input into App js to pass to Task js.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Task from './Components/Task';
 
 function App() {
+
+  const [taskName, setTaskName] = useState("")
+  const [time, setTime] = useState("")
+  const [taskList, setTaskList] = useState([])
+
+  function addTask() {
+    setTaskList([...taskList, {task: taskName, time: time}]);  //... means take existing stuff from taskList array and then add objects to it
+    setTaskName("");  //reset task name
+    setTime(""); //rest time
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <h1>TODO list</h1>
+     
+     <label>Task Name: </label>
+     <input type="text" id="text" onChange={(input) => setTaskName(input.target.value)} ></input>
+
+     <label>Time: </label>
+     <input type="text" id="time" onChange={(input) => setTime(input.target.value)}></input>
+
+     <button onClick={addTask}>Add</button>
+
+     {taskList.map(task => {
+       return <Task taskName={task.task} time={task.time}/>   //accessing object properties from taskList above
+     })}
+   </div> 
   );
 }
 
---
+++ src/Components/Task.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../App.css";
+
+
+export default function Task({taskName, time}) {
+    return (
+        <div className="task">
+            <h1>Task: {taskName}</h1>
+            <h1>Time to Complete: {time}</h1>
+            
+        </div>
+    );
+}
+
+ 
